c72f35228450f6ab59438166308907ba2a86e3ac
Ludo <ludoattuoni@gmail.com>
2021-11-24 09:31:16 +0000

    Tweaked scripts

--- Unity-Wii-Balanceboard/Wii-Balanceboard-server/bin/Debug/plot_weights.py
+++ Unity-Wii-Balanceboard/Wii-Balanceboard-server/bin/Debug/plot_weights.py
@@ -21,8 +21,40 @@ def plot(data_sets):
     plt.legend()
     plt.show()
     
-
-
+def plot_time(data_sets):
+    x_data = data_sets[4].split(",")
+    
+    xlabel = x_data[0]
+    x = list(map(float, x_data[1:]))
+    total = []
+    max_reading = []
+    for i in range(4):
+        y_data = data_sets[i].split(",")
+        ylabel = y_data[0]
+        
+        y = list(map(float, y_data[1:]))
+        avg_y = sum(y)/len(y)
+        y = list(map(lambda x : x - avg_y, y))
+        for j in range(len(y)):
+            if j >= len(total):
+                total.append(y[j])
+            else:
+                total[j] += y[j]
+            if j >= len(max_reading):
+                max_reading.append(y[j])
+            else:
+                max_reading[j] = y[j] if y[j] > max_reading[j] else max_reading[j]
+        plt.plot(x, y, label = ylabel)
+    
+    
+    plt.legend()
+    plt.show()
+    plt.plot(x,total,label = "Total readings")
+    #plt.plot(x,max_reading,label = "Highest readings")
+    plt.legend()
+    plt.show()
+plot_time(open("data.csv","r").read().split("\n"))
+'''
 names = ["dataBL.csv","dataTL.csv","dataBR.csv","dataTR.csv","data.csv"]
 
 
@@ -32,3 +64,4 @@ for name in names:
     data_sets = f.split('\n')
     plot(data_sets)
     
+'''
